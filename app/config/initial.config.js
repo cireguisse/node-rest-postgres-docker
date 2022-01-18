b6a0fbde33b4fd4e0930713f47f6106b3d976719
dalaba224 <guissepromiage20@gmail.com>
2022-01-18 01:20:08 +0100

    add helmet and update function init

--- app/config/initial.config.js
+++ app/config/initial.config.js
@@ -1,19 +1,19 @@
-module.exports = {
+const db = require('../models')
+const Role = db.role;
+
+function initial(){
+    Role.create({
+        id: 1,
+        name: "user"
+    });
     
-    function initial() {
-        Role.create({
-          id: 1,
-          name: "user"
-        });
-       
-        Role.create({
-          id: 2,
-          name: "moderator"
-        });
-       
-        Role.create({
-          id: 3,
-          name: "admin"
-        });
-    }
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+    
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
 }
--- server.js
+++ server.js
@@ -1,33 +1,52 @@
 
 const dotenv = require('dotenv');
 const express = require("express");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const http = require('http');
 const keepAliveAgent = new http.Agent({ keepAlive: true });
- const config = require('./app/config/initial.config');
+const config = require('./app/config/initial.config');
 const app = express();
 
 // get config vars
 dotenv.config();
 
-const db = require("./app/models");
-const Role = db.role;
 
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Db');
-  initial();
-});
+const db = require("./app/models");
+
+try {
+    db.sequelize.sync({force: true}).then(() => {
+        console.log('Drop and Resync Db');
+        config.initial();
+    });
+} catch (error) {
+    console.error(error);
+}
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+// Secure App express server
+app.use(helmet.contentSecurityPolicy());
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 
 app.use(cors(corsOptions));
 app.use(morgan('combined'));
-
-
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
